9398ffa65048da7d9212894e4f0bd36a5bbaebd7
James <jayninja@fazey.org>
2022-09-03 14:02:15 -0500

    figuring out how to add stuff

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+func formatAsString() string {
+	return "pow"
+}
 
 func main() {
 
 	r := gin.Default()
 	r.Use(gin.Logger())
 	r.Delims("{{", "}}")
+	r.SetFuncMap(template.FuncMap{
+		"formatAsString": formatAsString,
+	})
 	//ping
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -33,12 +42,26 @@ func main() {
 		// for each file
 		for _, file := range files {
 			fmt.Println(file.Name())
-			posts = append(posts, file.Name())
+			posts = append(posts, "markdown/"+file.Name())
 		}
 
 		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
 			"posts": posts,
 		})
+	})
+	r.GET("/markdown", func(c *gin.Context) {
+		contentTemp, err := os.ReadFile("markdown/hello-world.md")
+		if err != nil {
+			fmt.Println("File reading error", err)
+			return
+		}
+		content := string(contentTemp)
+
+		c.HTML(http.StatusOK, "test.tmpl.html", gin.H{
+			"content": content,
+		},
+		)
+
 	})
 
 	r.Run()
